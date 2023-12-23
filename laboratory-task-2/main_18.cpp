67dd74a876d3ca142e30e7539ba4190adddcebc2
VadimLabotsky <labotskyvadim@gmail.com>
2023-12-23 15:59:51 +0300

    Убрал пробелы между оператором цикла и скобкой. Добавил пробел в конце программы.

--- laboratory-task-2/main_18.cpp
+++ laboratory-task-2/main_18.cpp
@@ -21,18 +21,15 @@ int32_t findDigitInSequence(int32_t n, int32_t digitPosition)
     int32_t currentPosition = 0;
     int32_t numDigits = 1;
     int32_t temp = 0;
-        while (currentPosition < digitPosition)
-        {
+        while (currentPosition < digitPosition){
             temp = currentNumber;
-            while (temp >= 10)
-            {
+            while (temp >= 10){
                 temp /= 10;
                 numDigits++;
             }
             currentPosition += numDigits;
             currentNumber++;
-            if (currentPosition >= digitPosition)
-            {
+            if (currentPosition >= digitPosition){
                 return getDigit(currentNumber - 1, numDigits - (currentPosition - digitPosition));
             }
         }
@@ -48,12 +45,10 @@ int main()
     temp = digitPosition;
     int32_t digit = 0;
     digit = findDigitInSequence(temp, digitPosition);
-    if (digit != -1)
-    {
+    if (digit != -1){
         std::cout << "The digit with the number " << digitPosition << " in the sequence of natural numbers is equal to " << digit << std::endl;
     }
-    else
-    {
+    else{
         std::cout << "Digit with number " << digitPosition << " was not found in the natural sequence" << std::endl;
     }
     return 0;
