ea11c0ff50836b5950bed9787def96682d7da044
VadimLabotsky <labotskyvadim@gmail.com>
2024-01-13 23:09:24 +0300

    Добавил try, catch, функции, логическое разделение кода.

--- laboratory-task-2/main_2.cpp
+++ laboratory-task-2/main_2.cpp
@@ -8,31 +8,51 @@
 #include <iostream>
 
 
-int main()
+void getNumber(int32_t& number)
 {
-    int32_t number = 0; 
-    int32_t min = 9;
-    int32_t lastNumber = 0;
-    int32_t timeNumber = 0;
-    int32_t modifiedNumber = 0;
-    int32_t lastDigit = 0; 
-    double x = 0.1;
-    std::cout << "Input number\n";
-    std::cin >> number;
-    while (number <= 0) {
-        std::cout << "ERROR! Please, enter natural number!!! \n ";
+    bool indicator = false;
+
+    while (!indicator) {
+        std::cout << "Input number\n";
         std::cin >> number;
+
+        if (number < 0) {
+            std::cout << "ERROR!!!" << '\n';
+        }
+        if (!static_cast<char>(number)) {
+            throw std::exception("You have entered a symbol!");
+        }
+        else {
+            indicator = true;
+            break;
+        }
     }
-    timeNumber = number;
+}
+
+void minDigitInNumber(int32_t& number, int32_t& min)
+{
+    int32_t lastNumber = 0;
+    int32_t timeNumber = number;
+
     while (number) {
         lastNumber = number % 10;
         if (lastNumber < min) {
             min = lastNumber;
         }
         number /= 10;
-   
     }
+
     number = timeNumber;
+}
+
+void removesDigitsFromAMultipleMin(int32_t& number, int32_t& min)
+{
+    int32_t lastNumber = 0;
+    int32_t timeNumber = 0;
+    int32_t modifiedNumber = 0;
+    int32_t lastDigit = 0;
+    double x = 0.1;
+
     while (number > 0) {
         lastDigit = number % 10;
         number /= 10;
@@ -43,10 +63,28 @@ int main()
             modifiedNumber = modifiedNumber + (lastDigit * x);
         }
     }
-    number = timeNumber;
-    system("cls");
-    std::cout << number << '\n';
+
+    modifiedNumber = number;
     std::cout << "Modified number\n" << modifiedNumber;
+}
+
+int main()
+{
+    try {
+        int32_t number = 0;
+        int32_t min = 9;
+        int32_t lastNumber = 0;
+        int32_t timeNumber = 0;
+        int32_t modifiedNumber = 0;
+        int32_t lastDigit = 0;
+        double x = 0.1;
+
+        getNumber(number);
+        removesDigitsFromAMultipleMin(number, min);
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what();
+    }
     return 0;
 }
 
